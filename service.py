9c13b4326e9ecec258bb71bab2db140eda690b1b
Ryan King <ultradragonwarrior@gmail.com>
2021-10-15 11:25:08 -0600

    Added user name retrieval

---
+++ service.py
@@ -0,0 +1,15 @@
+"""
+Questions:
+- Will DBAccess be implemented as classes or just functions?
+- Are we using snake_case or camelCase for functions?
+- How do we want to handle missing data? Where do we want to handle it?
+"""
+
+# Get a user's name from their netID
+def getUserName(netId):
+  # FIXME: Replace this with whatever fancy function Derek comes up with
+  name = DBAccess.getUserName(netId)
+  if name == None:
+    name = "Student"
+
+  return name
