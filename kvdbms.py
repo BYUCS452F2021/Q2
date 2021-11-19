f5689a1ccf418ec92be86d5fc62fa8744dc67cef
Teikn Smith <spencerteiknsmith@gmail.com>
2021-11-18 17:10:02 -0700

    Implement kvdbms on the file system

--- kvdbms.py
+++ kvdbms.py
@@ -1,6 +1,24 @@
+import os
+import pickle
+
+
+def __key_to_file(key: str):
+    filepath = key.replace(':', '/')
+    directory, file = os.path.split(filepath)
+    os.makedirs(directory, exist_ok=True)
+    return filepath
+
+
 def get(key):
-    pass
+    file = __key_to_file(key)
+    with open(file, "rb") as f:
+        return pickle.load(f)
 
 
 def store(key, value):
-    pass
+    try:
+        file = __key_to_file(key)
+        with open(file, "wb") as f:
+            data = pickle.dump(value, f)
+    except:
+        raise "Must provide a pickleable object"
