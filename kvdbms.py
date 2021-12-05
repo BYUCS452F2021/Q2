c13a27c9b7047dc8fe2e1d79616d1f98f11811a9
Teikn Smith <spencerteiknsmith@gmail.com>
2021-12-04 18:28:05 -0700

    Use actual exceptions

--- kvdbms.py
+++ kvdbms.py
@@ -2,6 +2,14 @@ import os
 import pickle
 
 
+class InvalidKeyExpection(Exception):
+    pass
+
+
+class UnpicklableObjectException(Exception):
+    pass
+
+
 def __key_to_file(key: str):
     filepath = ".kvdbmsstore/" + key.replace(':', '/')
     directory, file = os.path.split(filepath)
@@ -15,7 +23,7 @@ def get(key):
         with open(file, "rb") as f:
             return pickle.load(f)
     except:
-        raise "Invalid key"
+        raise InvalidKeyExpection
 
 
 def store(key, value):
@@ -24,4 +32,4 @@ def store(key, value):
         with open(file, "wb") as f:
             data = pickle.dump(value, f)
     except:
-        raise "Must provide a pickleable object"
+        raise UnpicklableObjectException
